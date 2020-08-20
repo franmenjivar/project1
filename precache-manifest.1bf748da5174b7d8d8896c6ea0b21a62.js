e0f5a963dba2e2389b5ac05b07b38c184a87925e
franmenjivar <199fran5@gmail.com>
2020-08-19 23:22:23 -0600

    Updates

---
+++ precache-manifest.1bf748da5174b7d8d8896c6ea0b21a62.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b62fdd613c10352737829a00e7c25b8",
+    "url": "/project1/index.html"
+  },
+  {
+    "revision": "d369786f9b9137118cdb",
+    "url": "/project1/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "3e1d835c88976ae7c249",
+    "url": "/project1/static/css/main.fb5b8d46.chunk.css"
+  },
+  {
+    "revision": "d369786f9b9137118cdb",
+    "url": "/project1/static/js/2.89758641.chunk.js"
+  },
+  {
+    "revision": "6287c65a6963df837d60dc49bb220910",
+    "url": "/project1/static/js/2.89758641.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e1d835c88976ae7c249",
+    "url": "/project1/static/js/main.98cd4398.chunk.js"
+  },
+  {
+    "revision": "97a7bb867f3e56782e38",
+    "url": "/project1/static/js/runtime-main.653fe6a3.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/project1/static/media/SEGA.12a3db8b.woff"
+  }
+]);
